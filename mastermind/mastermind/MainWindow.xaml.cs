ed533a0242332c4b71506a7842983b84e9ab634f
ThibaultLesecquePXL <thibault.lesecque@student.pxl.be>
2024-11-17 17:43:34 +0100

    Mastermind-02

--- mastermind/mastermind/MainWindow.xaml.cs
+++ mastermind/mastermind/MainWindow.xaml.cs
@@ -26,7 +26,6 @@ namespace mastermind
         {
             InitializeComponent();
             pickColors();
-            comboBoxItemsInit();
         }
 
         private void pickColors()
@@ -37,14 +36,6 @@ namespace mastermind
             }
 
             this.Title = $"Mastermind ({colorSelectionString[colorsRandom[0]]}, {colorSelectionString[colorsRandom[1]]}, {colorSelectionString[colorsRandom[2]]}, {colorSelectionString[colorsRandom[3]]})";
-        }
-
-        private void comboBoxItemsInit()
-        {
-            color1ComboBox.ItemsSource = colorSelectionString;
-            color2ComboBox.ItemsSource = colorSelectionString;
-            color3ComboBox.ItemsSource = colorSelectionString;
-            color4ComboBox.ItemsSource = colorSelectionString;
         }
     }
 }
